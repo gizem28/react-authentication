cbae3f2b295f68f083fa02aa36c28686112a4f0e
gizem28 <gizemodun28@gmail.com>
2022-07-30 17:33:48 +0300

    first commit

---
+++ back-end/src/routes/index.js
@@ -0,0 +1,10 @@
+
+import { logInRoute } from './logInRoute';
+import { signUpRoute } from './signUpRoute';
+import { testRoute } from './testRoute';
+
+export const routes = [
+    logInRoute,
+    signUpRoute,
+    testRoute,
+];
